d3012cdd0626ed7a07547b29a8f27170ff00ce42
Arnaud Denagbe <arnaud.denagbe@synatec.de>
2015-10-02 12:03:24 +0200

    another formatting

--- src/de/xyr/reget/DownloadChooseDialog.java
+++ src/de/xyr/reget/DownloadChooseDialog.java
@@ -1,9 +1,11 @@
 package de.xyr.reget;
+
 import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
 import java.util.*;
 import javax.swing.*;
+
 /**
  * DownloadChooseDialog.java
  *
@@ -13,37 +15,32 @@ import javax.swing.*;
  * @author arnaud.denagbe@free.fr
  * @version 1.0
  */
-public class DownloadChooseDialog extends JDialog
-{
-    private Vector urls;
+public class DownloadChooseDialog extends JDialog {
+	private Vector urls;
 
-    public DownloadChooseDialog(Frame parent, File[] files) throws Exception
-    {
-	super(parent, "Downloads selector", true);
-	
-	urls = new Vector();
-	for(int i = 0; i < files.length; i++)
-	{
-	    String filename = files[i].getAbsolutePath();
-	    BufferedReader in = new BufferedReader(new FileReader(filename));
-	    for(String line; (line = in.readLine()) != null; )
-	    {
-		if (line.length() >= 4 && line.substring(0, 4).toLowerCase().equals("http"))
-		{
-		    urls.add(line);
-		};
-	    }
-	    in.close();
+	public DownloadChooseDialog(Frame parent, File[] files) throws Exception {
+		super(parent, "Downloads selector", true);
+
+		urls = new Vector();
+		for (int i = 0; i < files.length; i++) {
+			String filename = files[i].getAbsolutePath();
+			BufferedReader in = new BufferedReader(new FileReader(filename));
+			for (String line; (line = in.readLine()) != null;) {
+				if (line.length() >= 4 && line.substring(0, 4).toLowerCase().equals("http")) {
+					urls.add(line);
+				}
+				;
+			}
+			in.close();
+		}
+
+		/*
+		 * for(int i = 0; i < urls.size(); i++) {
+		 * System.out.println(urls.get(i)); }
+		 */
 	}
 
-	/*for(int i = 0; i < urls.size(); i++)
-	{
-	    System.out.println(urls.get(i));
-	    }*/
-    }
-
-    public Vector getUrls()
-    {
-	return urls;
-    }
+	public Vector getUrls() {
+		return urls;
+	}
 }// DownloadChooseDialog
--- src/de/xyr/reget/XyrRegetFileFilter.java
+++ src/de/xyr/reget/XyrRegetFileFilter.java
@@ -1,7 +1,9 @@
 package de.xyr.reget;
+
 import java.io.File;
 import javax.swing.*;
 import javax.swing.filechooser.*;
+
 /**
  * XyrRegetFileFilter.java
  *
@@ -11,56 +13,46 @@ import javax.swing.filechooser.*;
  * @author arnaud.denagbe@free.fr
  * @version 1.0
  */
-public class XyrRegetFileFilter extends FileFilter
-{
-    public final static String TEXT = "txt";
-    public final static String JPEG = "jpeg";
-    public final static String JPG = "jpg";
-    public final static String GIF = "gif";
-    public final static String TIFF = "tiff";
-    public final static String TIF = "tif";
+public class XyrRegetFileFilter extends FileFilter {
+	public final static String TEXT = "txt";
+	public final static String JPEG = "jpeg";
+	public final static String JPG = "jpg";
+	public final static String GIF = "gif";
+	public final static String TIFF = "tiff";
+	public final static String TIF = "tif";
 
-    // Accept all directories and all gif, jpg, or tiff files.
-    public boolean accept(File f)
-    {
-        if (f.isDirectory())
-	{
-            return true;
-        }
+	// Accept all directories and all gif, jpg, or tiff files.
+	public boolean accept(File f) {
+		if (f.isDirectory()) {
+			return true;
+		}
 
-        String extension = getExtension(f);
-        if (extension != null)
-	{
-            if (extension.equals(TEXT) || extension.equals(TIFF) || extension.equals(TIF) ||
-		extension.equals(GIF) || extension.equals(JPEG) || extension.equals(JPG))
-	    {
-                    return true;
-            }
-	    else
-	    {
-                return false;
-            }
-        }
+		String extension = getExtension(f);
+		if (extension != null) {
+			if (extension.equals(TEXT) || extension.equals(TIFF) || extension.equals(TIF) || extension.equals(GIF)
+					|| extension.equals(JPEG) || extension.equals(JPG)) {
+				return true;
+			} else {
+				return false;
+			}
+		}
 
-        return false;
-    }
-
-    private static String getExtension(File f)
-    {
-	String ext = null;
-	String s = f.getName();
-	int i = s.lastIndexOf('.');
-	
-	if (i > 0 &&  i < s.length() - 1)
-	{
-	    ext = s.substring(i+1).toLowerCase();
+		return false;
 	}
-	return ext;
-    }
-    
-    // The description of this filter
-    public String getDescription()
-    {
-        return "Just Images";
-    }
+
+	private static String getExtension(File f) {
+		String ext = null;
+		String s = f.getName();
+		int i = s.lastIndexOf('.');
+
+		if (i > 0 && i < s.length() - 1) {
+			ext = s.substring(i + 1).toLowerCase();
+		}
+		return ext;
+	}
+
+	// The description of this filter
+	public String getDescription() {
+		return "Just Images";
+	}
 }// XyrRegetFileFilter
